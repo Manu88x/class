95aa92e335df288f725fb4176e2002708b64c349
Manu88x <emmanuelokoth112@gmail.com>
2024-12-23 14:59:11 +0300

    code

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,33 @@
 class Song:
-    pass
+    # Class-level attributes to keep track of the counts
+    count = 0
+    genres = set()
+    artists = set()
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        
+        # Increment the total song count
+        Song.count += 1
+
+        # Track genres and artists
+        Song.genres.add(genre)
+        Song.artists.add(artist)
+        
+        # Track count of songs by genre
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+        
+        # Track count of songs by artist
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
+
+
